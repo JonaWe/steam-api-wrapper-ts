fc27e7faf34c096838d61b66cb81b97ed2334d0c
Jona Wessendorf <jonawessendorf@gmail.com>
2021-10-01 16:56:20 +0200

    adds playground

---
+++ src/sum.ts
@@ -0,0 +1,9 @@
+/**
+ * Calculates the sum of two numbers
+ * @param a first summand
+ * @param b second summand
+ * @returns the sum of  a and b
+ */
+export function sum(a: number, b: number) {
+  return a + b;
+}
