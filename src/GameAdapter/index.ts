3abd9a6141003c812c6001d78fd0d8bdb5d22826
Jona Wessendorf <jonawessendorf@gmail.com>
2021-10-19 19:56:38 +0200

    adds portal 1 and 2 game adapter

--- src/GameAdapter/index.ts
+++ src/GameAdapter/index.ts
@@ -1,3 +1,5 @@
 export { CSGOAdapter } from './730_CSGO/CSGOAdapter';
 export { TF2Adapter } from './440_TF2/TF2Adapter';
+export { PortalAdapter } from './400_Portal/PortalAdapter';
+export { Portal2Adapter } from './620_Portal2/Portal2Adapter';
 export { RustAdapter } from './252490_Rust/RustAdapter';
