c335d3a194c43a8cc03e0c3996e6b70ea47d722e
Jona Wessendorf <jonawessendorf@gmail.com>
2021-10-19 20:06:47 +0200

    adds pubg game adapter

--- src/GameAdapter/index.ts
+++ src/GameAdapter/index.ts
@@ -3,3 +3,4 @@ export { TF2Adapter } from './440_TF2/TF2Adapter';
 export { PortalAdapter } from './400_Portal/PortalAdapter';
 export { Portal2Adapter } from './620_Portal2/Portal2Adapter';
 export { RustAdapter } from './252490_Rust/RustAdapter';
+export { PUBGAdapter } from './578080_PUBG/PUBGAdapter';
