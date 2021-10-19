7085fa80613aebeb22d4a85c398adf8bfabc675d
Jona Wessendorf <jonawessendorf@gmail.com>
2021-10-19 20:10:28 +0200

    adds GTAV game adapter

--- src/GameAdapter/index.ts
+++ src/GameAdapter/index.ts
@@ -4,3 +4,4 @@ export { PortalAdapter } from './400_Portal/PortalAdapter';
 export { Portal2Adapter } from './620_Portal2/Portal2Adapter';
 export { RustAdapter } from './252490_Rust/RustAdapter';
 export { PUBGAdapter } from './578080_PUBG/PUBGAdapter';
+export { GTAVAdapter } from './271590_GTAV/GTAVAdapter';
